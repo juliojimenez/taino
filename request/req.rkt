cebf47d41b19ca2b5790ebc495e0de760728b52f
Julio Jimenez <julio@julioj.com>
2023-10-27 20:05:05 -0400

    so mvp right now
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- request/req.rkt
+++ request/req.rkt
@@ -1,5 +1,19 @@
 #lang racket
 
-(require web-server/http)
+;; This module provides the request struct.
 
-(provide request)
+(provide
+  ;; gets a request uri and resource
+  get-uri-resource)
+
+;; —————————————————————————————————
+;; import and implementation section
+
+(require 
+  ; web-server/http
+  web-server/servlet)
+
+(define (get-uri-resource r)
+  (let* ((uri (request-uri r))
+         (resource (map path/param-path (url-path uri))))
+    (list uri resource)))
--- server/serve.rkt
+++ server/serve.rkt
@@ -1,10 +1,18 @@
 #lang racket/base
 
-(require web-server/servlet-env)
+;; This module implements the web server.
+
+(provide
+  ;; launch server
+  server)
+
+;; —————————————————————————————————
+;; import and implementation section
+
+(require 
+  web-server/servlet-env)
 
 (define (server app [port 80])
   (serve/servlet app
     #:listen-ip "127.0.0.1"
     #:port port))
-
-(provide server)
